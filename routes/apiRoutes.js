1469863106b5a2e2d319c11e16d4a1c023441a56
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-27 02:09:34 -0500

    Delete button now functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,15 +9,27 @@ module.exports = function(app) {
     
     app.post("/api/notes", function(req, res) {
         var newNote = req.body;
-        
+
+        var newId;
+        if (notes.length === 0) {
+            newId = 0;
+        }
+        else {
+            newId = notes[notes.length - 1].id + 1;
+        }
+        newNote = {id:newId, ...newNote};
         notes.push(newNote);
-    
         console.log(notes);
-    
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes));
         res.json(newNote);
     });
     
     app.delete("/api/notes/:id", function(req, res) {
-        var deletedNote = req.params.id;
+        var deletedNoteId = parseInt(req.params.id);
+        var newNotes = notes.filter(note => note.id !== deletedNoteId);
+        notes = newNotes;
+        console.log(notes);
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+        res.json(notes);
     });
 };
